c500ac0740e276d2709a43a85637effa2f9bb005
DimitrovMiro <50617778+DimitrovMiro@users.noreply.github.com>
2021-03-19 13:27:16 +0200

    fix single sign on

--- elastic-install-seast-zone.sh
+++ elastic-install-seast-zone.sh
@@ -1059,7 +1059,7 @@ configure_elasticsearch_yaml()
           echo -e "  order: 2"
           echo -e "  idp.metadata.path: \"https://login.microsoftonline.com/afadec18-0533-4cba-8578-5316252ff93f/federationmetadata/2007-06/federationmetadata.xml?appid=91be6b22-9045-4147-8617-b3f6b02eff0e\""
           echo -e "  idp.entity_id: \"https://sts.windows.net/afadec18-0533-4cba-8578-5316252ff93f/\""
-          echo -e "  sp.entity_id:  \"$SAML_SP_URI/\""
+          echo -e "  sp.entity_id:  \"$SAML_SP_URI\""
           echo -e "  sp.acs: \"$SAML_SP_URI/api/security/v1/saml\""
           echo -e "  sp.logout: \"$SAML_SP_URI/logout\""
           echo -e "  attributes.principal: \"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name\""
